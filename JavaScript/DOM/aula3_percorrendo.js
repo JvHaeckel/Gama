8d031825b9b4c5c892faa56babbd7cb51532347c
JvHaeckel <joaohaeckel@gmail.com>
2022-12-14 08:24:39 -0300

    Aula 4

---
+++ JavaScript/DOM/aula3_percorrendo.js
@@ -0,0 +1,24 @@
+// Selecionar elemento
+
+
+// Pegar nodos filhos
+
+// Pegar primeiro filho da lista
+
+
+// Pegar o nome do node
+
+
+// Pegar valor (texto) do nodo
+
+
+// Pegar o tipo elemento
+
+
+// Pegar o elemento "pai"
+
+
+// Pegar primeiro elemento filho
+
+
+// Pegar o último elemento filho
