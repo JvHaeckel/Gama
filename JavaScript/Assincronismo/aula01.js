c4442ff1a39d6a9dfa9071fc7df6d8882a2fa037
JvHaeckel <joaohaeckel@gmail.com>
2022-12-20 08:25:54 -0300

    callback

---
+++ JavaScript/Assincronismo/aula01.js
@@ -0,0 +1,32 @@
+
+
+first = () => console.log("primeiro");
+second = () => {
+    setTimeout(() => console.log("second"), 500)
+}
+
+third = () => console.log("third");
+
+first();
+second();
+third();
+
+
+/*setTimeout - define um cronômetro que executa uma função ou trecho de código 
+ especificado assim que expira.No 1 parâmetro a função e no segundo o atraso
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/Assincronismo/callback.js
@@ -0,0 +1,16 @@
+
+
+// passo 1
+console.log('Passo 1');
+
+// passo 2
+console.log('Passo 2');
+
+// passo 3
+console.log('Passo 3');
+
+// passo 4
+console.log('Passo 4');
+
+
+
